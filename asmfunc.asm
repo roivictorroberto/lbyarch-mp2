dd55091c9b06d13c9209fdf7d44a8c30875469f1
Victor <110458160+roirobertooo@users.noreply.github.com>
2024-11-24 23:52:42 +0800

    cleaned up comments

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,5 +1,4 @@
 section .data
-    ; Constants for conversion
     kmh_to_ms: dq 0.277777778  ; 1000/3600 to convert km/h to m/s
 
 section .text
@@ -7,33 +6,25 @@ bits 64
 default rel
 global calculate_acceleration
 
-; Function to calculate acceleration
-;   rcx = pointer to matrix (double*)
-;   rdx = number of rows (int)
-;   r8  = pointer to results array (int*)
 calculate_acceleration:
     push rbp
     mov rbp, rsp
     
-    ; Save non-volatile registers we'll use
     push rbx
     push r12
     push r13
     push r14
     
-    ; Store parameters
     mov r12, rcx        ; matrix pointer
     mov r13, r8         ; results pointer
     mov ecx, edx        ; number of rows (copy lower 32 bits of rdx)
     
-    ; Load conversion constant into xmm15
     movsd xmm15, [rel kmh_to_ms]
     
-    ; Process each row
     xor rbx, rbx        ; row counter
     mov r14, 0          ; offset counter
 .loop:
-    ; Load initial velocity, final velocity, and time
+    ; Load values
     movsd xmm0, [r12 + r14]      ; initial velocity
     movsd xmm1, [r12 + r14 + 8]  ; final velocity
     movsd xmm2, [r12 + r14 + 16] ; time
@@ -59,7 +50,6 @@ calculate_acceleration:
     cmp ebx, ecx
     jl .loop
     
-    ; Restore registers and return
     pop r14
     pop r13
     pop r12
